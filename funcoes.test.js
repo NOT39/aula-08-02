223cb5e711dbbc421390dd269e08574b4ff3f2ab
not39 <eurique854@gmail.com>
2024-03-19 20:27:12 -0300

    Test: Criar teste de subtração

--- funcoes.test.js
+++ funcoes.test.js
@@ -1,10 +1,18 @@
 import { describe, it, expect } from 'vitest'
-import { soma } from './funcoes'
+import { soma, subtracao } from './funcoes'
 
 describe("Função de soma", () => {
   it("deve devolver a soma de dois números", () => {
     const resultado = soma(2, 2)
 
     expect(resultado).toBe(4)
+  })
+})
+
+describe("Funçaõ de subtração", () => {
+  it("deve devolver a subtração de dois números", () => {
+    const resultado = subtracao(5, 2)
+    
+    expect(resultado).toBe(3)
   })
 })
